cf1e576a57c729d6d7af80661f9f777abe5b0eb3
davidnon <darcila@lean-tech.io>
2022-04-10 22:05:45 -0500

    plant card modified fixes mobile

--- src/Components/PlantCard.tsx
+++ src/Components/PlantCard.tsx
@@ -4,6 +4,9 @@ import { IPlant } from "../Interfaces/IPlant";
 import { currencyFormat } from "../Utils/PipesNumber";
 import { Link } from "react-router-dom";
 import { dateDiffInDays } from "../Utils/CalcDiffDays";
+import { OverlayTrigger } from "react-bootstrap";
+import { popover } from "./Popover";
+import { FaCloudSun, FaMoon, FaSun } from "react-icons/fa";
 
 const PlantCard = ( {
                         plant,
@@ -16,7 +19,7 @@ const PlantCard = ( {
             if ( showWholesalePrices ) {
                 if ( discountWholesale > 0 ) {
                     return (
-                        <div className="d-flex justify-content-between">
+                        <div className="d-flex justify-content-between flex-wrap-reverse">
                             <div className="text-decoration-line-through text-muted">
                                 { currencyFormat( priceWholesale ) }
                             </div>
@@ -33,7 +36,7 @@ const PlantCard = ( {
                 }
             } else {
                 return (
-                    <div className="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between flex-wrap-reverse">
                         <div className="text-decoration-line-through text-muted">
                             Precio Oculto
                         </div>
@@ -43,7 +46,7 @@ const PlantCard = ( {
         } else if ( !wholesale ) {
             if ( discount > 0 ) {
                 return (
-                    <div className="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between flex-wrap-reverse">
                         <div className="text-decoration-line-through text-muted">
                             { currencyFormat( price ) }
                         </div>
@@ -70,13 +73,77 @@ const PlantCard = ( {
     return (
         <div className="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-3 mb-3">
             <div className="card">
-                { calcLastWeekPublished(plant.publishDate) &&  <div className="achievement-label">NUEVO</div> }
-                <img src={ `assets/${ plant.photos[0] }.png` } className="card-img-top img-size" alt="..."
-                     width="350" height="250"/>
+                { calcLastWeekPublished( plant.publishDate ) && <div className="achievement-label">NUEVO</div> }
+                <Link to={ `/detail-plant/${ plant.id }` }>
+                    <img src={ `../assets/${ plant.photos[0] }.png` } className="card-img-top img-size" alt="..."
+                         width="350" height="250"/>
+                </Link>
                 <div className="card-body">
                     <h5 className="card-title">{ plant.name }</h5>
-                    <p className="card-text text-truncate">{ plant.description }</p>
-                    <Link to={ `/detail-plant/${ plant.id }` } className="btn btn-primary">Ver Detalle</Link>
+                    <div className="d-flex flex-column gap-2">
+                        <section>
+                            <p className="card-text text-truncate">
+                                <strong>
+                                Descripcion: &nbsp;
+                            </strong>{ plant.description }
+                            </p>
+                        </section>
+                        <section>
+                            <strong>
+                                Cultivo:
+                            </strong>
+                            <span className="break-all fw-light"> {
+                                plant.cultivation.map( ( growing, index ) =>
+                                    <OverlayTrigger trigger="click" placement="right"
+                                                    key={ index }
+                                                    overlay={ popover(
+                                                        growing === 'full sun' ? "Sol" : ( growing === "shade" ? "Sombra" : "Semi-sombra" ),
+                                                        growing === 'full sun' ? [ "Esta planta sobrevive largas horas de",
+                                                                <strong> exposición solar </strong> ] :
+                                                            ( growing === "shade" ? [ "Esta planta sobrevive en ",
+                                                                        <strong>sombra</strong>, " con muy buena ",
+                                                                        <strong>sombra</strong>, " iluminación" ] :
+                                                                    [ "Esta planta sobrevive algunas horas de",
+                                                                        <strong> exposicion
+                                                                            solar</strong>, " y otras de ",
+                                                                        <strong>sombra</strong>, " con buena ",
+                                                                        <strong>iluminación</strong> ]
+                                                            ) ) }
+                                    >
+                                        <button key={ index }>
+                                            { growing === 'full sun' ?
+                                                <FaSun
+
+                                                    style={ {
+                                                        color: '#FFCF01',
+                                                        fontSize: '20px',
+                                                        marginRight: '0.5rem'
+                                                    } }
+                                                >
+                                                </FaSun>
+                                                :
+                                                ( growing === 'light shade' ?
+                                                        <FaCloudSun style={ {
+                                                            color: '#008EF7',
+                                                            fontSize: '20px',
+                                                            marginRight: '0.5rem'
+                                                        } }/> :
+                                                        <FaMoon style={ { fontSize: '16px' } }/>
+                                                ) }
+                                        </button>
+                                    </OverlayTrigger>
+                                ) }
+                                            </span>
+                        </section>
+                        <section>
+                            <strong>
+                                Tamaño: &nbsp;
+                            </strong>
+                            <span className="break-all fw-light">
+                         { plant.size.join( ', ' ) }
+                    </span>
+                        </section>
+                    </div>
                 </div>
                 <div className="card-footer bg-light border-light fw-bold">
                     {
