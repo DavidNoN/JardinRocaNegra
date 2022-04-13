86fe6ef18e6a845542779108b02653262c553e11
David Arcila Parra <david.arcilap@perficient.com>
2022-04-13 18:35:15 -0500

    change navbar color

--- src/Shared/Navbar.tsx
+++ src/Shared/Navbar.tsx
@@ -1,15 +1,15 @@
-import React, { LegacyRef } from 'react';
-import { Link, NavLink } from "react-router-dom";
+import React, {LegacyRef} from 'react';
+import {Link, NavLink} from "react-router-dom";
 import '../index.css';
 import '../Styles/NavBar.scss';
-import { MenuItems } from "../Components/MenuApp";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {MenuItems} from "../Components/MenuApp";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 
 const NavBar = () => {
     return (
-        <header className="header px-3 py-3 gradient-gray" id="header">
+        <header className="header px-3 py-3 bg-orange" id="header">
             <nav className="navbar container">
                 <Link to="/new-plants"
                       className="col-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto col-xxl-auto me-sm-2 me-md-3 me-lg-auto text-white text-decoration-none justify-se mb-3">
@@ -24,7 +24,7 @@ const NavBar = () => {
                                aria-label="Recipient's username" aria-describedby="button-addon2"/>
                         <button className="btn btn-outline" type="button"
                                 id="button-addon2">
-                            <FontAwesomeIcon icon={ faSearch }/>
+                            <FontAwesomeIcon icon={faSearch} style={{color: "white"}}/>
                         </button>
                     </div>
                 </form>
@@ -32,20 +32,20 @@ const NavBar = () => {
                 <div className="menu" id="menu">
                     <ul className="menu-list">
                         {
-                            MenuItems.map( ( menuItem, index ) => {
+                            MenuItems.map((menuItem, index) => {
 
                                 const Icon = menuItem.component;
 
-                                return <li key={ index } className="menu-item">
+                                return <li key={index} className="menu-item">
 
-                                    <NavLink to={ menuItem.url } aria-current="page" className={ ( { isActive } ) =>
+                                    <NavLink to={menuItem.url} aria-current="page" className={({isActive}) =>
                                         isActive ? 'is-active nav-link menu-link menu-item-active' : 'nav-link text-black-50'
                                     }>
-                                        <Icon { ...menuItem.props }  />
-                                        { menuItem.name }
+                                        <Icon {...menuItem.props}  />
+                                        {menuItem.name}
                                     </NavLink>
                                 </li>
-                            } )
+                            })
                         }
                     </ul>
                 </div>
