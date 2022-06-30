2185aa092aca25d46828221751be42be62f7f6fc
DavidNoN <darcila@lean-tech.io>
2022-06-29 20:37:32 -0500

    added carousel for plant img

--- src/Components/PlantCard.tsx
+++ src/Components/PlantCard.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import '../Styles/PlantCard.scss'
-import { IPlant } from "../Interfaces/IPlant";
-import { currencyFormat } from "../Utils/PipesNumber";
-import { Link } from "react-router-dom";
-import { dateDiffInDays } from "../Utils/CalcDiffDays";
-import { OverlayTrigger } from "react-bootstrap";
-import { popover } from "./Popover";
-import { FaCloudSun, FaMoon, FaSun } from "react-icons/fa";
+import {IPlant} from "../Interfaces/IPlant";
+import {currencyFormat} from "../Utils/PipesNumber";
+import {dateDiffInDays} from "../Utils/CalcDiffDays";
+import {OverlayTrigger} from "react-bootstrap";
+import {popover} from "./Popover";
+import {FaCloudSun, FaMoon, FaSun} from "react-icons/fa";
+import Carousel from "../Screens/Carousel";
 
-const PlantCard = ( {
-                        plant,
-                        wholesale,
-                        showWholesalePrices
-                    }: { plant: IPlant, wholesale: boolean, showWholesalePrices: boolean } ) => {
+const PlantCard = ({
+                       plant,
+                       wholesale,
+                       showWholesalePrices
+                   }: { plant: IPlant, wholesale: boolean, showWholesalePrices: boolean }) => {
 
-    const calcPrice = ( price: number, priceWholesale: number, discount: number, discountWholesale: number, wholesale: boolean, showWholesalePrices: boolean ) => {
-        if ( wholesale ) {
-            if ( showWholesalePrices ) {
-                if ( discountWholesale > 0 ) {
+    const calcPrice = (price: number, priceWholesale: number, discount: number, discountWholesale: number, wholesale: boolean, showWholesalePrices: boolean) => {
+        if (wholesale) {
+            if (showWholesalePrices) {
+                if (discountWholesale > 0) {
                     return (
                         <div className="d-flex justify-content-between flex-wrap-reverse">
                             <div className="text-decoration-line-through text-muted">
-                                { currencyFormat( priceWholesale ) }
+                                {currencyFormat(priceWholesale)}
                             </div>
                             <div>
-                                { currencyFormat( priceWholesale * ( 1 - discountWholesale ) ) }
+                                {currencyFormat(priceWholesale * (1 - discountWholesale))}
                             </div>
                             <div className="text-success">
-                                { discountWholesale * 100 }%
+                                {discountWholesale * 100}%
                             </div>
                         </div>
                     )
                 } else {
-                    return currencyFormat( priceWholesale );
+                    return currencyFormat(priceWholesale);
                 }
             } else {
                 return (
@@ -43,54 +43,52 @@ const PlantCard = ( {
                     </div>
                 )
             }
-        } else if ( !wholesale ) {
-            if ( discount > 0 ) {
+        } else if (!wholesale) {
+            if (discount > 0) {
                 return (
                     <div className="d-flex justify-content-between flex-wrap-reverse">
                         <div className="text-decoration-line-through text-muted">
-                            { currencyFormat( price ) }
+                            {currencyFormat(price)}
                         </div>
                         <div>
-                            { currencyFormat( price * ( 1 - discount ) ) }
+                            {currencyFormat(price * (1 - discount))}
                         </div>
                         <div className="text-success">
-                            { ( discount * 100 ) }%
+                            {(discount * 100)}%
                         </div>
-                    </div> )
+                    </div>)
             } else {
-                return currencyFormat( price );
+                return currencyFormat(price);
             }
         }
     }
 
-    const calcLastWeekPublished = ( date: string ): boolean => {
-        const plantDate = new Date( date );
+    const calcLastWeekPublished = (date: string): boolean => {
+        const plantDate = new Date(date);
         const currentDate = new Date();
-        const diffDays = dateDiffInDays( plantDate, currentDate );
+        const diffDays = dateDiffInDays(plantDate, currentDate);
         return diffDays <= 7;
     }
 
     return (
-        <div className="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-3 mb-3">
+        <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-3 mb-3">
             <div className="card">
                 {
-                    calcLastWeekPublished( plant.publishDate ) && plant.state ?
-                    <div className="achievement-label">NUEVO</div> : ( !plant.state && <div className="out-stock-label">AGOTADO</div> )
+                    calcLastWeekPublished(plant.publishDate) && plant.state ?
+                        <div className="achievement-label">NUEVO</div> : (!plant.state &&
+                            <div className="out-stock-label">AGOTADO</div>)
 
                 }
-                <Link to={ `/detail-plant/${ plant.id }` }>
-                    <img src={ `assets/${ plant.photos[0] }.png` } className="card-img-top img-size" alt="planImg"
-                         loading="lazy"
-                         width="350" height="250"/>
-                </Link>
+                <Carousel plantPhotos={plant.photos} />
+
                 <div className="card-body">
-                    <h5 className="card-title">{ plant.name }</h5>
+                    <h5 className="card-title">{plant.name}</h5>
                     <div className="d-flex flex-column gap-2">
                         <section>
                             <p className="card-text text-truncate">
                                 <strong>
-                                Descripcion: &nbsp;
-                            </strong>{ plant.description }
+                                    Descripcion: &nbsp;
+                                </strong>{plant.description}
                             </p>
                         </section>
                         <section>
@@ -98,46 +96,48 @@ const PlantCard = ( {
                                 Cultivo:
                             </strong>
                             <span className="break-all fw-light"> {
-                                plant.cultivation.map( ( growing, index ) =>
+                                plant.cultivation.map((growing, index) =>
                                     <OverlayTrigger trigger="click" placement="right"
-                                                    key={ index }
-                                                    overlay={ popover(
-                                                        growing === 'full sun' ? "Sol" : ( growing === "shade" ? "Sombra" : "Semi-sombra" ),
-                                                        growing === 'full sun' ? [ "Esta planta sobrevive largas horas de",
-                                                                <strong> exposición solar </strong> ] :
-                                                            ( growing === "shade" ? [ "Esta planta sobrevive en ",
+                                                    key={index}
+                                                    overlay={popover(
+                                                        growing === 'full sun' ? "Sol" : (growing === "shade" ? "Sombra" : "Semi-sombra"),
+                                                        growing === 'full sun' ? ["Esta planta sobrevive largas horas de",
+                                                                <strong> exposición solar </strong>] :
+                                                            (growing === "shade" ? ["Esta planta sobrevive en ",
                                                                         <strong>sombra</strong>, " con muy buena ",
-                                                                        <strong>sombra</strong>, " iluminación" ] :
-                                                                    [ "Esta planta sobrevive algunas horas de",
+                                                                        <strong>sombra</strong>, " iluminación"] :
+                                                                    ["Esta planta sobrevive algunas horas de",
                                                                         <strong> exposicion
                                                                             solar</strong>, " y otras de ",
                                                                         <strong>sombra</strong>, " con buena ",
-                                                                        <strong>iluminación</strong> ]
-                                                            ) ) }
-                                    >
-                                        <button key={ index }>
-                                            { growing === 'full sun' ?
+                                                                        <strong>iluminación</strong>]
+                                                            ))}
+                                     >
+                                        <button key={index}>
+                                            {growing === 'full sun' ?
                                                 <FaSun
-
-                                                    style={ {
+                                                    key={index}
+                                                    style={{
                                                         color: '#FFCF01',
                                                         fontSize: '20px',
                                                         marginRight: '0.5rem'
-                                                    } }
+                                                    }}
                                                 >
                                                 </FaSun>
                                                 :
-                                                ( growing === 'light shade' ?
-                                                        <FaCloudSun style={ {
+                                                (growing === 'light shade' ?
+                                                        <FaCloudSun
+                                                            key={index}
+                                                            style={{
                                                             color: '#008EF7',
                                                             fontSize: '20px',
                                                             marginRight: '0.5rem'
-                                                        } }/> :
-                                                        <FaMoon style={ { fontSize: '16px' } }/>
-                                                ) }
+                                                        }}/> :
+                                                        <FaMoon style={{fontSize: '16px'}}/>
+                                                )}
                                         </button>
                                     </OverlayTrigger>
-                                ) }
+                                )}
                                             </span>
                         </section>
                         <section>
@@ -145,14 +145,14 @@ const PlantCard = ( {
                                 Tamaño: &nbsp;
                             </strong>
                             <span className="break-all fw-light">
-                         { plant.size.join( ', ' ) }
+                         {plant.size.join(', ')}
                     </span>
                         </section>
                     </div>
                 </div>
                 <div className="card-footer bg-light border-light fw-bold">
                     {
-                        calcPrice( plant["price-collector"], plant[`price-wholesale`], plant.discount, plant[`discount-wholesale`], wholesale, showWholesalePrices )
+                        calcPrice(plant["price-collector"], plant[`price-wholesale`], plant.discount, plant[`discount-wholesale`], wholesale, showWholesalePrices)
                     }
                 </div>
             </div>
--- src/Screens/Carousel.tsx
+++ src/Screens/Carousel.tsx
@@ -1,55 +1,104 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../index.css';
 import '../Styles/Carousel.scss';
+import {Link} from "react-router-dom";
 
-const Carousel = ( { plantPhotos }: { plantPhotos: string[] } ) => {
+const Carousel = ({plantPhotos}: { plantPhotos: string[] }) => {
+
+    const [indexPicture, setIndexPicture] = useState(0);
+
+    const validateChangePicture = (state: string, index: number) => {
+        if (state === 'prev') {
+            setIndexPicture(indexPicture - 1);
+        } else if (state === 'next' && indexPicture <= plantPhotos.length) {
+            setIndexPicture(indexPicture + 1);
+        } else if (state === 'indicator') {
+            setIndexPicture(index);
+        } else {
+            return;
+        }
+    }
+
+
 
     return (
-        <div id="carouselExampleCaptions" className="carousel mb-3" data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" data-interval="false" className="carousel mb-3 carousel-resize">
             <div className="carousel-indicators">
                 {
-                    plantPhotos.map( ( photo, index ) =>
-                        index === 0 ?
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={ index }
-                                    className="active"
-                                    key={ index }
-                                    aria-current="true" aria-label={ `Slide ${ index + 1 }` }/> :
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={ index }
-                                    key={ index }
-                                    aria-current="true" aria-label={ `Slide ${ index + 1 }` }/>
+                    plantPhotos.map((photo, index) =>
+                        plantPhotos.length > 1 ?
+                            index === indexPicture ?
+                                <button type="button"
+                                        data-bs-target="#carouselExampleCaptions"
+                                        data-bs-slide-to={indexPicture}
+                                        className="active"
+                                        onClick={() => validateChangePicture('indicator', index)}
+                                        key={index}
+                                        aria-current="true"
+                                        aria-label={`Slide ${indexPicture + 1}`}/> :
+                                <button type="button"
+                                        data-bs-target="#carouselExampleCaptions"
+                                        data-bs-slide-to={indexPicture}
+                                        onClick={() => validateChangePicture('indicator', index)}
+                                        key={index}
+                                        aria-current="true"
+                                        aria-label={`Slide ${indexPicture + 1}`}/> :
+                            <></>
                     )
 
                 }
             </div>
             <div className="carousel-inner">
 
-                { plantPhotos.map( ( photo, index ) =>
+                {plantPhotos.map((photo, index) =>
 
-                    index === 0 ?
+                    index === indexPicture ?
                         <div className="carousel-item active">
-                            <img src={ `assets/${ photo }.png` }
-                                 className="d-block full-width"
-                                 style={ { objectFit: "cover" } } alt="plantPhoto"
-                                 key={ index }/>
+
+                            <img src={`assets/${plantPhotos[indexPicture]}.png`}
+                                 className="d-block img-height"
+                                 style={{objectFit: "cover"}} alt="plantPhoto"
+                                 width="350" height="250"
+                                 loading="lazy"
+                                 key={index}/>
                         </div> :
                         <div className="carousel-item">
-                            <img src={ `assets/${ photo }.png` }
-                                 className="d-block full-width"
-                                 style={ { objectFit: "cover" } } alt="plantPhoto"
-                                 key={ index }/>
+                            <img src={`assets/${plantPhotos[indexPicture]}.png`}
+                                 className="d-block img-height"
+                                 style={{objectFit: "cover"}} alt="plantPhoto"
+                                 width="350" height="250"
+                                 loading="lazy"
+                                 key={index}/>
                         </div>
-                ) }
+                )}
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                    data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"/>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                    data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"/>
-                <span className="visually-hidden">Next</span>
-            </button>
+            {
+                plantPhotos.length > 1 &&
+                <>
+                    {
+                        indexPicture > 0 &&
+                        <button className="carousel-control-prev"
+                                type="button"
+                                onClick={() => validateChangePicture('prev', 0)}
+                                data-bs-target="#carouselExampleCaptions"
+                                data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"/>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                    }
+                    {
+                        indexPicture < plantPhotos.length - 1 &&
+                        <button className="carousel-control-next"
+                                type="button"
+                                data-bs-target="#carouselExampleCaptions"
+                                onClick={() => validateChangePicture('next', 0)}
+                                data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"/>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    }
+                </>
+            }
         </div>
     );
 };
