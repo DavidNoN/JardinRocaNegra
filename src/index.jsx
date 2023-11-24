f105f1a59e85fdf5851209eed32e3ab8e16c229a
David <davidnon@utp.edu.co>
2023-11-24 16:18:40 -0500

    Delete src directory

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.css';
-
-import ReactDom from 'react-dom/client';
-import BlackRockApp from "./BlackRockApp";
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<BlackRockApp/>)
